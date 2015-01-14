687b05bbdceb39227c5918bb3714a2d3a590cc96
Matt Koken <maskoken@gmail.com>
2015-01-14 01:22:23 +0000

    homework 1

--- HMWK/HMWK1/HW1_2.cpp
+++ HMWK/HMWK1/HW1_2.cpp
@@ -6,32 +6,89 @@
 using namespace std;
 
 statistician::statistician() {
-	
+	_length = 0;
+	_lastNumber = 0;
+	_smallest = 0;
+	_sum = 0;
+	_largest = 0;
 }
 void statistician::next_number(double number) {
-	
+	_lastNumber = number;
+	_sum+=number;
+	if(_length > 0) {
+	    if (_smallest > number) {
+    	    _smallest = number
+	    }
+        if (largest < number) {
+            _largest = number;
+	    }
+	}
+	else {
+	    _smallest = number;
+	    _largest = number;
+	}
+	_length++;
 }
 double statistician::getLastNumber() {
-	
+    assert(_length>0);
+	return _lastNumber;
 }
 double statistician::getLargestNumber() {
-	
+    assert(_length>0);
+	return _largest;
 }
 double statistician::getSmallestNumber() {
-	
+    assert(_length>0);
+	return _smallest;
 }
 double statistician::getMean() {
-	
+    assert(_length>0);
+	return _sum/_length;
 }
 double statistician::getSum() {
-	
+	return _sum;
 }
 long statistician::length() {
-	
+	return _length;
 }
 void statistician::reset() {
-	
+	_length = 0;
+	_lastNumber = 0;
+	_sum = 0;
+	_smallest = 0;
+	_largest = 0;
 }
 statistician statistician::operator+ (statistician b) {
+	statician newStatician = new statistician();
+	newStatician._length = _length + b.length();
+	newStatician._sum = _sum + b.getSum();
 	
+	
+	if(_length > 0 && b._length > 0) {
+	
+    	if(_smallest > b.getSmallestNumber()) {
+    	    newStatician._smallest = b.getSmallestNumber();
+    	}
+    	else {
+    	    newStatician._smallest = _smallest;
+    	}
+        if(_largest > b.getLargestNumber()) {
+	        newStatician._largest = _largest;
+	    }
+    	else {
+	        newStatician._largest = b.getLargestNumber();
+	    }
+	}
+	else if (_length == 0) {
+	    newStatician._smallest = b.getSmallestNumber();
+	    newStatician._largest = b.getLargestNumber();
+	    newStatician._lastNumber = b.getLastNumber();
+	}
+	else { //b must be length zero
+	    newStatician._smallest = _smallest;
+	    newStatician._largest = _largest;
+	    newStatician._lastNumber = _lastNumber;
+	}    
+
+	return newStatician;
 }
--- HMWK/HMWK1/HW1_8.cpp
+++ HMWK/HMWK1/HW1_8.cpp
@@ -9,45 +9,133 @@ int quadratic::_numberOfRealRoots(double* sol1, double* sol2) {
 }
 
 quadratic::quadratic(double a, double b, double c) {
-	
+	_a = a;
+	_b = b;
+	_c = c;
 }
 
 void quadratic::updateCoeffcients(double a, double b, double c) {
-	
+	_a = a;
+	_b = b;
+	_c = c;
 }
 
 double quadratic::getA() {
-	
+	return _a;
 }
 
 double quadratic::getB() {
-	
+	return _b;
 }
 
 double quadratic::getC() {
-	
+	return _c;
 }
 
 double quadratic::evaluate(double x) {
-	
+	return (_a * x * x) + (_b * x) + _c
+}
+
+int _numberOfRealRoots(double* sol1, double* sol2) {
+	if(_a==0) {
+		if(_b==0) {
+			if(_c==0;) { //case 1
+				if(sol!=NULL) {
+					*sol1=0;
+				}
+				if(sol2!=NULL) {
+					*sol2=0;
+				}
+				return 3;
+			}
+			else { //case 2
+				return 0;
+			}
+		}
+		else { //case 3
+			if(sol1!=NULL) {
+				*sol1 = (-_c/_b);
+			}
+			if(sol2!=NULL() {
+				*sol2 = (-_c/_b);
+			}
+			return 1;
+		}
+	}
+	else {
+		double b2 = _b * _b;
+		double _4ac = 4 * _a * _c;
+		
+		if(b2<_4ac) { //case 4
+			if(*sol1!=NULL) {
+				*sol1 = 0;
+			}
+			if(*sol2!=NULL) {
+				*sol2 = 0;
+			}
+			return 0;
+		}
+		if(b2 == _4ac) { //case 5
+			if(*sol1!=NULL) {
+				*sol1 = -_b / (2 * _a);
+			}
+			if(*sol2!=NULL) {
+				*sol2 = -_b / (2 * _a);
+			}
+			return 1;
+		}
+		if(b2 > _4ac) { //case 6
+			if(*sol1!=NULL) {
+				*sol1 = (-_b - sqrt(_b * _b - 4 * _a * _c))/(2 * _a)
+			}
+			if(*sol2!=NULL) {
+				*sol2 = (-_b + sqrt(_b * _b - 4 * _a * _c))/(2 * _a)
+			}
+			return 2;
+		}
+		
+	}
+	return;
 }
 
 int quadratic::numberOfRealRoots() {
-	
+	return _numberOfRealRoots(NULL, NULL);
 }
 
 double quadratic::smallerSolution() {
-	
+	double sol1, sol2;
+	_numberOfRealRoots(sol1, sol2);
+	if(sol1 > sol2) {
+		return sol2;
+	}
+	else {
+		return sol2;
+	}
 }
 
 double quadratic::largerSolution() {
-	
+	double sol1, sol2;
+	_numberOfRealRoots(sol1, sol2);
+	if(sol1 < sol2) {
+		return sol2;
+	}
+	else {
+		return sol1;
+	}
 }
 
 quadratic operator+ (const quadratic& q1, const quadratic& q2) {
-	
+	quadratic q = new quadratic();
+	q._a = (q1._a + q2._a);
+	q._b = (q1._b + q2_b);
+	q._c = q1._c + q2._c;
+	return q;
 }
 
 quadratic operator * (double r, const quadratic& q) {
-	
+	quadratic newQ = new quadratic();
+	newQ._a = r * q._a;
+	newQ._b = r * q._b;
+	newQ._c = r * q._c;
+	return q;
 }
