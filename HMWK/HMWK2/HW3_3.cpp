0750627dc8901549e143f517e57def6347951796
Khaos Tian <khaos.tian@gmail.com>
2015-01-19 12:00:32 -0800

    Imp

--- HMWK/HMWK2/HW3_3.cpp
+++ HMWK/HMWK2/HW3_3.cpp
@@ -13,7 +13,12 @@ sequence::sequence() {
     current_index = -1;
 }
 void sequence::start() {
-    
+    if (used > 0)
+	{
+		current_index = 0;
+	} else {
+		current_index = -1;
+	}
 }
 void sequence::advance() {
     if(current_index )
@@ -28,13 +33,18 @@ void sequence::remove_current() {
     
 }
 size_type sequence::size() const {
-    
+    return used;
 }
 bool sequence::is_item() const {
-    
+    return (current_index != -1);
 }
 value_type sequence::current() const{
-    
+    if (is_item())
+    {
+    	return data[current_index];
+    } else {
+    	return -1;
+    }
 }
 void sequence::add_front(const value_type& entry) {
     
