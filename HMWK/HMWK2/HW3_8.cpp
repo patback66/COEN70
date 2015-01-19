399c003873dc54ae7ebf88505593dff974c05f7e
Khaos Tian <khaos.tian@gmail.com>
2015-01-19 13:25:34 -0800

    Setup implementation

---
+++ HMWK/HMWK2/HW3_8.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "HW3_8.h"
+
+using namespace std;
+
+keyed_bag::keyed_bag(int cap) {
+
+}
+
+keyed_bag::~keyed_bag() {
+
+}
+
+keyed_bag::keyed_bag(const keyed_bag& other) {
+
+}
+
+keyed_bag& keyed_bag::operator=(const keyed_bag& rhs) {
+
+}
+
+ostream& operator<<(ostream&, const keyed_bag& rhs) {
+
+}
+
+void keyed_bag::insert(const double& value, int key) {
+
+}
+
+void keyed_bag::remove(int key) {
+
+}
+
+bool keyed_bag::keyExist(int key) {
+
+}
