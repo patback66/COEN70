5085a8c7a0394bd78e5e4de6685a9699bcb545f5
Khaos Tian <khaos.tian@gmail.com>
2015-01-19 13:30:40 -0800

    Update headers

--- HMWK/HMWK2/HW3_8.cpp
+++ HMWK/HMWK2/HW3_8.cpp
@@ -6,11 +6,20 @@ using namespace std;
 keyed_bag::keyed_bag(int cap) {
 	_size = 0;
 	_cap = cap;
-	
+	_hashTable = new item*[cap];
 }
 
 keyed_bag::~keyed_bag() {
+	for (int i = 0; i < cap; ++i)
+	{
+		
+	}
+}
 
+void keyed_bag::deallocItems(item* obj) {
+	while(obj -> next != NULL)
+		deallocItems(obj -> next);
+	delete obj;
 }
 
 keyed_bag::keyed_bag(const keyed_bag& other) {
