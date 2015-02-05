3acfca3b562920bec5091372126f76fec83f24b6
Khaos Tian <khaos.tian@gmail.com>
2015-02-05 15:13:56 -0800

    7.13

--- HMWK/HMWK4/HW7_13.cpp
+++ HMWK/HMWK4/HW7_13.cpp
@@ -4,9 +4,54 @@
 using namespace std;
 
 class newStack: public stack {
-	
+	public:
+		friend bool compare(newStack&, newStack&);
 };
 
-int main(int argc, char *argv[]) {
+bool compare(newStack& s1, newStack& s2) {
+	if (s1._size != s2._size) {
+		return false;
+	}
 	
+	if (s1._size == 0 && s1._size == 0) {
+		return true;
+	}
+	
+	if (s1._size == 0 && s2._size > 0) {
+		return false;
+	}
+	
+	if (s1._size > 0 && s2._size == 0) {
+		return false;
+	}
+	
+	newStack::node* s1n = s1._head -> _next;
+	newStack::node* s2n = s2._head -> _next;
+	
+	while (s1n != NULL) {
+		if (s1n -> _data != s2n -> _data) {
+			return false;
+		}
+		s1n = s1n -> _next;
+		s2n = s2n -> _next;
+	}
+	
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+	newStack a,b,c;
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	
+	b.push(1);
+	b.push(2);
+	b.push(3);
+	
+	c.push(2);
+	c.push(3);
+	
+	cout << compare(a, b) << endl;
+	cout << compare(a, c) << endl;
 }
