59ab8d7a0949c95f4528c25f17131122a8f4c08c
Matt Koken <maskoken@gmail.com>
2015-02-24 18:12:57 -0800

    Class Notes/Review for midterm

--- LABS/lab6/main.cpp
+++ LABS/lab6/main.cpp
@@ -17,18 +17,17 @@ int main(int argc, const char * argv[])
     //case: insert duplicate key
     m.insert("first", 3);
     m.insert("second", 2);
-    
+
     //case: check contains existing key
     m.contains_key("first");
     //case: check contains nonexistant key
     m.contains_key("third");
-    
+
     //case: get value of existing key
     cout << m.value_of("first") << endl; //expected: 1
     //case: get value of nonexistant key
     cout << m.value_of("third") << endl; //expected: not found, \n 0
-    
+
     m.remove_key("first"); //case: remove existing key
     m.remove_key("third"); //case: remove key that doesn't exist
 }
-
