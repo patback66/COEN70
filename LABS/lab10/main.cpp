35266f08b18e8f1b425730b56e298e84d12a5741
Matt Koken <maskoken@gmail.com>
2015-03-10 19:02:12 +0000

    lab10 progress

---
+++ LABS/lab10/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  lab10
+//
+//  Created by matthew alexander koken on 3/9/15.
+//  Copyright (c) 2015 matthew alexander koken. All rights reserved.
+//
+
+#include <iostream>
+#include "graph.h"
+
+int main(int argc, const char * argv[])
+{
+
+    graph maze = *new graph(3, 3);
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
